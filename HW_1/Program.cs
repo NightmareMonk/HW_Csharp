22fce7037c5a036be0f351d8adbb0101b61bc6b3
Kirill <Nightmare643@outlook.com>
2023-09-24 17:07:07 +0300

    неважное изменение

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -11,7 +11,7 @@ int num2 = int.Parse(Console.ReadLine());
 if (num1 > num2)
 {
     Console.WriteLine(num1 + " большее");
-    Console.Write(num2 + " меньшее");
+    Console.WriteLine(num2 + " меньшее");
 }
 
 else if (num1 == num2)

a529c4a135e2abac900ebaec69e5aae4463b27a5
Kirill <Nightmare643@outlook.com>
2023-09-24 17:04:25 +0300

    неважное изменение

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -10,19 +10,19 @@ Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
 if (num1 > num2)
 {
-    Console.WriteLine(+ num1 + " большее");
-    Console.Write(+ num2 + " меньшее");
+    Console.WriteLine(num1 + " большее");
+    Console.Write(num2 + " меньшее");
 }
 
 else if (num1 == num2)
 
 {
-    Console.WriteLine(+ num1 + " равно " + num2);
+    Console.WriteLine(num1 + " равно " + num2);
 }
 else
 {
-    Console.Write(+ num2 + " большее");
-    Console.Write(+ num1 + " меньшее");
+    Console.Write(num2 + " большее");
+    Console.Write(num1 + " меньшее");
 }
 
     }

e2244b26ac20eddfcba9c413d0133fb5ca3808d2
Kirill <Nightmare643@outlook.com>
2023-09-24 16:52:49 +0300

    Добавил третье ДЗ

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+        Console.WriteLine("четное");
+}
+else
+{
+        Console.WriteLine("нечетное");
+}
+    }
+}

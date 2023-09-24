5914ad79b61ffa4b46a222049fbe55fccca18eef
Kirill <Nightmare643@outlook.com>
2023-09-24 17:00:03 +0300

    Добавил четвертое ДЗ

---
+++ HW_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int i = 2;
+
+while (num >= i)
+{
+    Console.Write(i + " ");
+    i = i + 2;
+}
+    }
+}
